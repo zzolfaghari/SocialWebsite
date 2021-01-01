6a94ca8d90222cc5626d3c2c48dde332614f0d88
zara <zarazolfaghari76@gmail.com>
2021-01-01 15:06:08 +0330

    added login form and view

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+
+                else:
+                    return HttpResponse('disabled account')
+            else:
+                return HttpResponse('Invalid login')
+
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
