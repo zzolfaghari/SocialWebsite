42b5c5313bb73a1c60b7fe092a1dfdf28638cdb7
zara <zarazolfaghari76@gmail.com>
2021-01-18 17:47:51 +0330

    add message framework

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth import authenticate, login
 from .models import Profile
+from django.contrib import messages
 
 
 def user_login(request):
@@ -58,6 +59,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
