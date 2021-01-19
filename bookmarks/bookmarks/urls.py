a8a7217c90bd17c54f49b3ea47a04b855769ccd2
zara <zarazolfaghari76@gmail.com>
2021-01-19 18:26:56 +0330

    add images app url

--- bookmarks/bookmarks/urls.py
+++ bookmarks/bookmarks/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls'))
+    path('account/', include('account.urls')),
+    path('images/', include('images.urls', namespace='images')),
 ]
 
 if settings.DEBUG:
