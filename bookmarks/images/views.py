1f4d4f6e034c97ff7ca94f05f3c7c3d45583fb1e
zara <zarazolfaghari76@gmail.com>
2021-01-20 17:57:24 +0330

    import utils

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from actions.utils import create_action
 
 
 @login_required
@@ -15,6 +16,7 @@ def image_create(request):
             new_item = form.save(commit=False)  # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
             # redirect to new created item detail view
             return redirect(new_item.get_absolute_url())
