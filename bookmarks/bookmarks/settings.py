543eaef9b35e23d78e9df5323451c718f099b9da
zara <zarazolfaghari76@gmail.com>
2021-01-20 16:28:01 +0330

    add ABSOLUTE_URL_OVERRIDES

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.urls import reverse_lazy
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -53,6 +55,11 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'bookmarks.urls'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail',
+                                        args=[u.username])
+}
 
 TEMPLATES = [
     {
