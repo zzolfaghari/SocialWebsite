77326d18deee989143445f9f35ebca92d8af9b4a
zara <zarazolfaghari76@gmail.com>
2021-01-22 11:26:18 +0330

    add user_follow path

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -17,4 +17,6 @@ urlpatterns = [
     path('edit/', views.edit, name='edit'),
     path('users/', views.user_list, name='user_list'),
     path('users/<username>/', views.user_detail, name='user_detail'),
+    path('users/follow/', views.user_follow, name='user_follow'),
+
 ]
