88bf165bfc98275d77867abf362b70e7cfd2370c
zara <zarazolfaghari76@gmail.com>
2021-01-22 11:12:42 +0330

    add total_likes to image model

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -12,6 +12,8 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
+    total_likes = models.PositiveIntegerField(db_index=True,
+                                              default=0)
 
     def __str__(self):
         return self.title
