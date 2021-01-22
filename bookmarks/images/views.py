45cd684c4894bb79c67186679bc60cb5a440c273
zara <zarazolfaghari76@gmail.com>
2021-01-22 20:46:37 +0330

    add image_list view and refactor

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import ImageCreateForm
-from actions.utils import create_action
-from .models import Image
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import ImageCreateForm
+from .models import Image
+from common.decorators import ajax_required
+from actions.utils import create_action
 import redis
 from django.conf import settings
-from django.shortcuts import get_object_or_404
 
 # connect to redis
-r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+r = redis.StrictRedis(host=settings.REDIS_HOST,
+                      port=settings.REDIS_PORT,
+                      db=settings.REDIS_DB)
 
 
 @login_required
@@ -22,21 +26,24 @@ def image_create(request):
         if form.is_valid():
             # form data is valid
             cd = form.cleaned_data
-            new_item = form.save(commit=False)  # assign current user to the item
+            new_item = form.save(commit=False)
+
+            # assign current user to the item
             new_item.user = request.user
             new_item.save()
             create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
+
             # redirect to new created item detail view
             return redirect(new_item.get_absolute_url())
     else:
         # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
-        return render(request,
-                      'images/image/create.html',
-                      {'section': 'images',
 
-                       'form': form})
+    return render(request,
+                  'images/image/create.html',
+                  {'section': 'images',
+                   'form': form})
 
 
 def image_detail(request, id, slug):
@@ -44,11 +51,15 @@ def image_detail(request, id, slug):
     # increment total image views by 1
     total_views = r.incr('image:{}:views'.format(image.id))
     # increment image ranking by 1
-    r.zincrby('image_ranking', image.id, 1)
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image,
-                                                        'total_views': total_views})
+    r.zincrby('image_ranking', 1, image.id)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image,
+                   'total_views': total_views})
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
@@ -57,26 +68,51 @@ def image_like(request):
     if image_id and action:
         try:
             image = Image.objects.get(id=image_id)
-            create_action(request.user, 'likes', image)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
-            return JsonResponse({'status': 'ok'})
+            return JsonResponse({'status':'ok'})
         except:
             pass
-    return JsonResponse({'status': 'ko'})
+    return JsonResponse({'status':'ko'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request,
+                      'images/image/list_ajax.html',
+                      {'section': 'images', 'images': images})
+    return render(request,
+                  'images/image/list.html',
+                   {'section': 'images', 'images': images})
 
 
 @login_required
 def image_ranking(request):
     # get image ranking dictionary
-    image_ranking = r.zrange('image_ranking', 0, -1,
-                             desc=True)[:10]
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
     image_ranking_ids = [int(id) for id in image_ranking]
     # get most viewed images
     most_viewed = list(Image.objects.filter(
-        id__in=image_ranking_ids))
+                           id__in=image_ranking_ids))
     most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
     return render(request,
                   'images/image/ranking.html',
