08d4568df55f379d4022c06f01a50b8cd2941756
zara <zarazolfaghari76@gmail.com>
2021-02-13 14:21:49 +0330

    refactored models

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -8,7 +8,7 @@ class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
-    url = models.URLField()
+    url = models.URLField(max_length=400)
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
