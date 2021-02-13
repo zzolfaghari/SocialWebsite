9efafebfa40e9b291c0f3b88dd984382a99839d8
zara <zarazolfaghari76@gmail.com>
2021-02-13 14:39:28 +0330

    refactored views

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,35 +1,35 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .models import Profile
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_POST
-# from common.decorators import ajax_required
-from .models import Contact
-from actions.utils import create_action
-from actions.models import Action
+from django.contrib.auth.models import User
+from bookmarks.common.decorators import ajax_required
+from bookmarks.actions.utils import create_action
+from bookmarks.actions.models import Action
+from .forms import LoginForm, UserRegistrationForm, \
+    UserEditForm, ProfileEditForm
+from .models import Profile, Contact
 
 
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            data = form.cleaned_data
-            user = authenticate(request, username=data['username'], password=data['password'])
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
                     return HttpResponse('Authenticated successfully')
-
                 else:
-                    return HttpResponse('disabled account')
+                    return HttpResponse('Disabled account')
             else:
                 return HttpResponse('Invalid login')
-
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
@@ -37,14 +37,19 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # Display all actions by default
     actions = Action.objects.exclude(user=request.user)
-    following_ids = request.user.following.values_list('id',
-                                                       flat=True)
+    following_ids = request.user.following.values_list('id', flat=True)
     if following_ids:
         # If user is following others, retrieve only their actions
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions.select_related('user', 'user__profile')[:10]
-    return render(request, 'account/dashboard.html', {'section': 'dashboard', 'actions': actions})
+    actions = actions.select_related('user', 'user__profile') \
+                  .prefetch_related('target')[:10]
+
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard',
+                   'actions': actions})
 
 
 def register(request):
@@ -58,19 +63,27 @@ def register(request):
                 user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            # Create the user profile
             Profile.objects.create(user=new_user)
             create_action(new_user, 'has created an account')
-            return render(request, 'account/register_done.html', {'new_user': new_user})
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request, 'account/register.html', {'user_form': user_form})
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
 
 
 @login_required
 def edit(request):
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user, data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
@@ -81,7 +94,10 @@ def edit(request):
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
 
 
 @login_required
@@ -104,7 +120,7 @@ def user_detail(request, username):
                    'user': user})
 
 
-# /@ajax_required
+@ajax_required
 @require_POST
 @login_required
 def user_follow(request):
@@ -114,11 +130,12 @@ def user_follow(request):
         try:
             user = User.objects.get(id=user_id)
             if action == 'follow':
-                Contact.objects.get_or_create(user_from=request.user, user_to=user)
-
-
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+                create_action(request.user, 'is following', user)
             else:
-                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
             return JsonResponse({'status': 'ok'})
         except User.DoesNotExist:
             return JsonResponse({'status': 'ko'})
