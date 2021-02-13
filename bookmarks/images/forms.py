a004b3286e4deee202da830376734362147109f4
zara <zarazolfaghari76@gmail.com>
2021-02-13 14:22:23 +0330

    refactored form

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -9,17 +9,14 @@ class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ('title', 'url', 'description')
-        widgets = {
-            'url': forms.HiddenInput,
-        }
 
     def clean_url(self):
         url = self.cleaned_data['url']
 
-        valid_extensions = ['jpg', 'jpeg']
+        # valid_extensions = ['jpg', 'jpeg', 'png']
         extension = url.rsplit('.', 1)[1].lower()
-        if extension not in valid_extensions:
-            raise forms.ValidationError('The given URL does not match valid image extensions.')
+        # if extension not in valid_extensions:
+        #     raise forms.ValidationError('The given URL does not match valid image extensions.')
         return url
 
     def save(self, force_insert=False, force_update=False, commit=True):
