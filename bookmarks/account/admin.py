fe9e1138bd84a7ccb46bd4e34baaf742ef0acc46
zahra00 <zahrazolfaghari00@gmail.com>
2021-07-08 12:11:48 +0430

    add Contact Admin to admin panel

--- bookmarks/account/admin.py
+++ bookmarks/account/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Profile
+from .models import Profile, Contact
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth', 'photo']
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['user_from']
