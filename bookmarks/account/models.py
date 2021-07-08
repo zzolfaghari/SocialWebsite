63f15af0b0dd8b9ed631c5ec7e83e6ffce10f7d7
zahra00 <zahrazolfaghari00@gmail.com>
2021-07-08 12:09:14 +0430

    fix bug on photo field

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(blank=True, upload_to='users/%Y/%m/%d/')
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
